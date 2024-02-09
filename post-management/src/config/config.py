ad0a448a58723d8e2e377475ac73afa840b2a06d
Diego Becerra <d.becerraz@uniandes.edu.co>
2024-02-09 00:32:02 -0500

    set to test

--- post-management/src/config/config.py
+++ post-management/src/config/config.py
@@ -3,11 +3,11 @@ import sys
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER") or 'postgres'
+DB_PASSWORD = os.getenv("DB_PASSWORD") or 'postgres'
+DB_HOST = os.getenv("DB_HOST") or 'localhost'
 DB_PORT = os.getenv("DB_PORT") or '5432'
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("DB_NAME") or 'post_management'
 USERS_PATH = os.getenv("USERS_PATH")
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)

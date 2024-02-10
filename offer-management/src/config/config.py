2887be17e6425b15c30af277475af373cb6500fa
Mateo_Caceres <m.caceresa23@uniandes.edu.co>
2024-02-10 09:24:10 -0500

    corrigiendo

--- offer-management/src/config/config.py
+++ offer-management/src/config/config.py
@@ -3,11 +3,11 @@ import sys
 from dotenv import load_dotenv
 
 load_dotenv()
-DB_USER = os.getenv("DB_USER") or 'postgres'
-DB_PASSWORD = os.getenv("DB_PASSWORD") or '1234'
-DB_HOST = os.getenv("DB_HOST") or 'localhost'
-DB_PORT = os.getenv("DB_PORT") or '5432'
-DB_NAME = os.getenv("DB_NAME") or 'ofertas'
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 USERS_PATH = os.getenv("USERS_PATH")
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
